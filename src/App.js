c6d7e3f3b6f74e1a498e4b83d5cfa28ff6caa48c
sputnix <spudnikex@gmail.com>
2024-09-23 16:58:17 -0500

    Chapter 2 - App & JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Components are represented by functions
 function App() {
+  // Crate a new javascript variable, can be used inside JSX return statement.
+  // JSX renders strings and integers as text in the web view.
+  const name = "Drake";
+  const number = 10;
+    // To use these variables inside JSX, must use { }.
+      // The use of { } signifies the use of a Javascript expression.
+
+  /* 
+  Functions are created using const keyword
+  Standard practice is to create functions using arrow functions = () => {}
+  */
+  const handleNameChange = () => {
+    const names = ["bob","kevin","dave"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
+
+  // The return statement inside functions (or "components") returns JSX.
+  // JSX allows the use of Javascript expressions (ex: {logo}).
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +38,22 @@ function App() {
         >
           Learn React
         </a>
+        <p>{/* This is how to include a comment, using JS Expression in JSX */}</p>
+        <p>
+          {/* Functions are called in expressions */}
+          {handleNameChange()}
+          </p>
+        <p>
+          {/* Variables can be passed into JSX with Javascript expressions */}
+          String: {name} 
+          Integer: {number}
+          Array: [0,1,2]
+          Javascript expression array: {[0,1,2]}
+        </p>
       </header>
     </div>
   );
 }
 
+// 
 export default App;
