33dc89a4d2131525da373ed10f2270b238e001ae
sputnix <spudnikex@gmail.com>
2024-09-23 13:06:49 -0700

    Init

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// In javascript, get the root id file. This is where React will inject components, in the next block below.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+  App is a "component".
+  Components are essentially seperate .js files, that include the methods and functionality
+    Apps are written in JSX, a mixture of HTML and Javascript
+*/ 
+
+// Can CTRL + click on the component to quick navigate to its respective .js file.
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
