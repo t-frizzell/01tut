98f35aac786d634174d9c4f3c36ea8ac5703720b
sputnix <spudnikex@gmail.com>
2024-09-23 18:01:02 -0500

    Ch4 - Inline CSS Style

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,17 @@
 import React from 'react'
 
 const Header = () => {
+    // This is an example of inline css
+    // To use inline, use an "object" variable
+    const headerStyle = {
+        backgroundColor: 'royalblue',
+        color: '#fff',
+    }
+
     return (
-        <header>
+        <header style={headerStyle}>
+        {/* Use a Js Expression to assign the value of the above object to the header style */}
+
         {/* 
         Best practice to use semantic HTML
         For this component, instead of <div>, use <header>
